90e29df0a5e3f981903b54a28d8e1656336100b8
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-02-15 11:53:18 +0530

    python
    intro

---
+++ intro.py
@@ -0,0 +1,52 @@
+#arithmetic operators
+#+-/*%.... arithmetic opetators
+
+#+plus operator
+a=10
+b=20
+total=a+b
+print(total)
+
+aakash=10000
+sasi=20000
+vedha=30000
+print(aakash+sasi+vedha)
+
+#-minus operator
+a=10
+b=20
+total=a-b
+print(total)
+#== assignment operator
+
+a=10
+b=20
+c=10
+equal=(a==c)
+print(equal)
+
+#*minus operator
+a=10
+b=20
+total=a*b
+print(total)
+
+'''
+string=words(sasi,mani,abc)
+integer=number(87,2888736)
+float=0.1,9.4
+'''
+
+a2="s"
+print(type(a2))
+
+b=987677
+print(type(b))
+c=7.8
+print(type(c))
+
+tamil=98
+english=60
+maths=78
+total=tamil+english-maths
+print(total)
